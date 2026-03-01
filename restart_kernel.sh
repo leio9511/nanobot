3381937defda5832c8f1291b62df1ee63e26c756
Leio Chen <leio.chen@gmail.com>
2026-03-01 08:56:43 +0000

    update restart_kernel.sh

--- restart_kernel.sh
+++ restart_kernel.sh
@@ -4,15 +4,26 @@
 PACKAGE="com.google.clawminium.kernel"
 SERVICE=".KernelService"
 ADB_TARGET="192.168.72.155:46869"
+USER_ID="10"
 
 echo "Connecting to ADB target: $ADB_TARGET..."
 adb connect $ADB_TARGET
 
-echo "Restarting AgentKernel Service..."
+echo "Restarting AgentKernel Service for user $USER_ID..."
 # Stop the service first to ensure a clean start
-adb shell am stopservice -n $PACKAGE/$SERVICE
+adb shell am stopservice --user $USER_ID -n $PACKAGE/$SERVICE
 sleep 1
 # Start as foreground service
-adb shell am start-foreground-service -n $PACKAGE/$SERVICE
+adb shell am start-foreground-service --user $USER_ID -n $PACKAGE/$SERVICE
+
+echo "Fetching internal bridge IP address..."
+BRIDGE_IP=$(adb -s $ADB_TARGET shell ip -4 addr show avf_tap_fixed | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+
+if [ -z "$BRIDGE_IP" ]; then
+    echo "Could not find IP on avf_tap_fixed. Showing all IPs:"
+    adb -s $ADB_TARGET shell ip -4 addr show | grep 'inet '
+else
+    echo "AgentKernel SSE endpoint URL: http://$BRIDGE_IP:8080/sse"
+fi
 
 echo "Done."
